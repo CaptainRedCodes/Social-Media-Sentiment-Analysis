faacddcb87d76746b0d480971a3bb1eebfb851db
CaptainRedCodes <108347458+CaptainRedCodes@users.noreply.github.com>
2023-12-05 17:46:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+#if __name__ == '__main__':
+    #app.run(debug=False,host='0.0.0.0')
